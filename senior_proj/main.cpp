61422d68e3ad4cf779c46f6d5ef1bbfbbe4c3be1
adamrt918 <thiemann.adam@gmail.com>
2024-02-07 21:12:39 -0500

    Tutorial

--- senior_proj/main.cpp
+++ senior_proj/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window *window = SDL_CreateWindow("Hello SDL World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Window *window = SDL_CreateWindow("Solemn Evolution", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
 
     if ( NULL == window )
     {
