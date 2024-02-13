a14cb8f7135a0fdcd9c1ac8f3893bae1c923e6c9
adamrt918 <thiemann.adam@gmail.com>
2024-02-12 21:09:58 -0500

    wip -- Can't figure out multiple textures

--- senior_proj/main.cpp
+++ senior_proj/main.cpp
@@ -6,6 +6,9 @@
 #include <string>
 #include <cmath>
 #include <../ltexture.hpp>
+#include <vector>
+
+using namespace std;
 
 //Background color black {r, g, b}
 const SDL_Color BACKGROUND_COLOR = {0, 0, 0};
@@ -28,9 +31,18 @@ void close();
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
 
-//Rendered texture
+//Rendered global texture and Text Rectangles
 LTexture gTextTexture;
+LTexture lTextTexture[4];
+SDL_Rect selection[4];
 
+//The page number that is being rendered to the texture.
+int pgNum = 0;
+
+//DELETE IF NECESSARY Messing around, text vector and texture vector
+vector<string> mmVector;
+vector<SDL_Texture*> tVector;
+std::vector<SDL_Rect> wordRects;
 
 int main( int argc, char* args[] )
 {
@@ -40,19 +52,46 @@ int main( int argc, char* args[] )
 
     //Load media
     if( !loadMedia() )
-			printf( "Failed to load media!\n" );
-
-    //Get the monitor dimensions
-
+        printf( "Failed to load media!\n" );
 
     //Main loop flag
-    bool quit = false;
+    bool gaming = true;
 
     //Event handler
     SDL_Event e;
 
+    
+        /*BEGIN DELETE*/
+        // // Split text into words
+        // std::string text = "Begin Tutorial Survey Exit";
+        // std::vector<std::string> words;
+        // std::string word;
+        // for (char c : text) {
+        //     if (c == ' ') {
+        //         words.push_back(word);
+        //         word.clear();
+        //     } else {
+        //         word += c;
+        //     }
+        // }
+        // if (!word.empty())
+        //     words.push_back(word);
+        // // for (int i = 0; i < words.size(); i++)
+        // // {
+        // //     cout << "i: " << i << "\tword: " << words[i] << endl;
+        // // }
+        // // Calculate individual word positions and render textures
+        // int textX;
+        // int textY;
+        // //This will only load the last word because it is part of a loop
+        // for (int i = 0; i < words.size(); i++)
+        // {
+
+        // }
+        /*END DELETE*/
+
     //While application is running
-    while( !quit )
+    while( gaming )
     {
         //Handle events on queue
         while( SDL_PollEvent( &e ) != 0 )
@@ -60,12 +99,11 @@ int main( int argc, char* args[] )
             //User requests quit
             if( e.type == SDL_QUIT )
             {
-                quit = true;
+                gaming = false;
             }
         }
-
         //Clear screen
-        SDL_SetRenderDrawColor( gTextTexture.gRenderer, 0, 0, 0, 255 );
+        SDL_SetRenderDrawColor( gTextTexture.gRenderer, BACKGROUND_COLOR.r, BACKGROUND_COLOR.g, BACKGROUND_COLOR.b, 255 );
         SDL_RenderClear( gTextTexture.gRenderer );
 
         //Render current frame
@@ -77,7 +115,6 @@ int main( int argc, char* args[] )
 	
 	//Free resources and close SDL
 	close();
-
 	return 0;
 }
 
@@ -106,7 +143,7 @@ bool init()
     }
 
     //Create window
-    gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, dimensions.w, dimensions.h, SDL_WINDOW_SHOWN );
+    gWindow = SDL_CreateWindow( "A Solemn Evolution", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, dimensions.w, dimensions.h, SDL_WINDOW_SHOWN );
     if( gWindow == NULL )
     {
         printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
@@ -143,28 +180,30 @@ bool init()
 
 bool loadMedia()
 {
-	//Loading success flag
-	bool success = true;
-
 	//Open the font
-	gTextTexture.gFont = TTF_OpenFont( "resources/Abadi_MT_Std.ttf", 28 );
+        //Need to put a variable for font size depending on the page
+	gTextTexture.gFont = TTF_OpenFont( "resources/Abadi_MT_Std.ttf", 72 );
 	if( gTextTexture.gFont == NULL )
 	{
-		printf( "Failed to load lazy font! SDL_ttf Error: %s\n", TTF_GetError() );
-		success = false;
-	}
-	else
-	{
-		//Render text
-		SDL_Color textColor = { 255, 255, 255 };
-		if( !gTextTexture.loadFromRenderedText( "The quick brown fox jumps over the lazy dog", textColor ) )
-		{
-			printf( "Failed to render text texture!\n" );
-			success = false;
-		}
+		printf( "Failed to load font! SDL_ttf Error: %s\n", TTF_GetError() );
+		return false;
 	}
 
-	return success;
+    //Render text, determines the page that is loaded.
+        //I need to create a function to format and change text sizes based on
+        //what I want to display
+        //
+        //The following function can size text
+        //SDL_TTF.SizeText(game.font, text_cstring, &text.rect.w, &text.rect.h)
+
+   
+    if( !gTextTexture.loadFromRenderedText("Begin Tutorial Survey Exit", TEXT_COLOR ) )
+    {
+        printf( "Failed to render text texture!\n" );
+        return false;
+    }
+   
+	return true;
 }
 
 void close()
