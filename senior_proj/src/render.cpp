d4d200cb41a30ad9ff889d00d0416ffa037c2970
adamrt918 <thiemann.adam@gmail.com>
2024-03-16 14:10:17 -0400

    More text pages

--- senior_proj/src/render.cpp
+++ senior_proj/src/render.cpp
@@ -13,9 +13,8 @@ void Render::choicePageRenderer(int chooser, SDL_Renderer* renderer, Player* gam
 {
     dimensions.initWindow();
     if (!gamer->getInsane())
-    for (int i = 0; i < CHOICE_PAGE_TEXTURES; i++){
-        textures[i].render(dimensions.w() / 2 - textures[i].getWidth() / 2, dimensions.h() / 2 - textures[i].getHeight() + totalHeight(i) + (i*20), NULL, 0, NULL, SDL_FLIP_NONE, renderer);
-        }
+        for (int i = 0; i < CHOICE_PAGE_TEXTURES; i++)
+            textures[i].render(dimensions.w() / 2 - textures[i].getWidth() / 2, dimensions.h() / 2 - textures[i].getHeight() + totalHeight(i) + (i*20), NULL, 0, NULL, SDL_FLIP_NONE, renderer);
     else{
         textures[0].render(dimensions.w() / 2 - textures[0].getWidth() / 2, dimensions.h() / 2 - textures[0].getHeight() + totalHeight(0) + (0*20), NULL, 0, NULL, SDL_FLIP_NONE, renderer);
         textures[1].render(dimensions.w() / 2 - textures[1].getWidth() / 2, dimensions.h() / 2 - textures[1].getHeight() + totalHeight(1) + (1*20), NULL, 0, NULL, SDL_FLIP_NONE, renderer);
